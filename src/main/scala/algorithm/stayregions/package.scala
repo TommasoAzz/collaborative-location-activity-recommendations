089f6a68c7bc79901d6425d4d5bc805ad14c21a8
Tommaso Azzalin <azzalintommaso@gmail.com>
2022-01-30 17:26:08 +0100

    Fixed mean neighbour ratio

--- src/main/scala/algorithm/stayregions/package.scala
+++ src/main/scala/algorithm/stayregions/package.scala
@@ -13,7 +13,7 @@ import scala.collection.mutable.ListBuffer
 
 package object stayregions {
   def computeStayRegions(gridCells: RDD[GridCell])(partitioning: Partitioning): RDD[StayRegion] = {
-    val neighbourRatios = new ListBuffer[Seq[Double]]
+    // val neighbourRatios = new ListBuffer[Seq[Double]]
 
     val results = partitioning match {
       case Partitionings.GridCell => gridCells
@@ -21,26 +21,28 @@ package object stayregions {
         .partitionBy(new GridCellPartitioner(SparkProjectConfig.DEFAULT_PARALLELISM))
         .mapPartitions(pairs => {
           val result = _computeStayRegions(pairs.map(_._2))
-          neighbourRatios += result._2
-          println(s"Partition mean neighbour ratio: ${result._2.sum / result._2.size} (${result._2.sum}, ${result._2.size})")
-          result._1
+          // neighbourRatios += result._2
+          // println(s"Partition mean neighbour ratio: ${result._2.sum / result._2.size} (${result._2.sum}, ${result._2.size})")
+          result
         })
       case Partitionings.Hash => gridCells
         .mapPartitions(cells => {
           val result = _computeStayRegions(cells)
-          neighbourRatios += result._2
-          result._1
+          // neighbourRatios += result._2
+          result
         })
     }
 
-    val ratios = neighbourRatios.filter(_.nonEmpty).map(ratios => ratios.sum / ratios.size)
+    // val ratios = neighbourRatios.filter(_.nonEmpty).map(ratios => ratios.sum / ratios.size)
+    val nonNullRatios = results.map(_._2).filter(_ >= 0.0)
+    val meanRatio = nonNullRatios.sum / nonNullRatios.count
 
-    println(s"Mean neighbour ratios: ${ratios}")
+    println(s"Mean neighbour ratios: $meanRatio")
 
-    results
+    results.map(_._1)
   }
 
-  private def _computeStayRegions(gridCells: Iterator[GridCell]): (Iterator[StayRegion], Seq[Double]) = {
+  private def _computeStayRegions(gridCells: Iterator[GridCell]): Iterator[(StayRegion, Double)] = {
     val sortedCells = gridcells.sortGridCells(gridCells.toSeq)
 
     if (sortedCells.isEmpty) {
@@ -52,7 +54,8 @@ package object stayregions {
       if !sortedCells(i).assigned // if not already assigned to a stay region
     } yield computeStayRegion(i, sortedCells)).filter(_._3 >= AlgorithmConfig.MIN_NUM_STAY_POINTS_PER_REGION)
 
-    (stayRegionsAndRatios.map(_._1).iterator, stayRegionsAndRatios.map(_._2))
+    // (stayRegionsAndRatios.map(_._1).iterator, stayRegionsAndRatios.map(_._2).iterator)
+    stayRegionsAndRatios.map(sr => (sr._1, sr._2)).iterator
   }
 
   private def computeStayRegion(index: Int, gridCells: Seq[GridCell]): (StayRegion, Double, Int) = {
